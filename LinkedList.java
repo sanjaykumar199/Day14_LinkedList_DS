0b3d795405999ccf8b626beaeb790c4f8b7d4504
sanjay kumar <sanjaykumar14021998@gmail.com>
2022-10-21 20:27:46 -0500

    uc1-creat-linked-list

--- LinkedList.java
+++ LinkedList.java
@@ -1,8 +1,27 @@
 package Com.bridgelab.day14_LinkedList;
 
 public class LinkedList {
-    public static void main(String[] args) {
-        System.out.println("Welcome to LinkedList program");
+    Node head;
+    class Node
+    {
+        Object data;
+        Node ref;
+        Node(Object data)
+        {
+            this.data = data;
+        }
+    }
+    public void elements(Object data)
+    {
+        Node newNode = new Node(data);
+        System.out.print(newNode.data+" ");
+    }
 
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        System.out.println("Welcome to LinkedList program");
+        list.elements(56);
+        list.elements(30);
+        list.elements(70);
     }
 }

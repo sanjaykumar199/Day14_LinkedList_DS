9794c4e81cf053ac72db85b62c8c19823798dada
sanjay kumar <sanjaykumar14021998@gmail.com>
2022-10-21 20:24:55 -0500

    Day14_LinkedList

---
+++ LinkedList.java
@@ -0,0 +1,8 @@
+package Com.bridgelab.day14_LinkedList;
+
+public class LinkedList {
+    public static void main(String[] args) {
+        System.out.println("Welcome to LinkedList program");
+
+    }
+}
